ba7d7a494cc99836b61e958a180aec223fb29b03
lelekaspb <bov-1986@yandex.ru>
2021-10-23 16:48:14 +0200

    added animations on front page

--- js/model.js
+++ js/model.js
@@ -44,7 +44,7 @@ const model = (function () {
       name: "hogwarts student list",
       imageSrc: "assets/images/hogwarts.jpg",
       overview:
-        "A very interesting and challenging individual project. The app fetches two files with the use of Asynchronous JavaScript functions, corrects students' names, constructs image sources, calculates blood statuses, and displays it all in browser. In addition, the app allows users to search, filter, and sort students. The most challenging part of the assignment was to implement a function that would hack the system. The function can be called from the console (hackTheSystem();) or by typing 'hackit' while app is open in browser. It adds myself as a student and won't allow my expelling, it also randomizes blood statuses and makes sure that students added to the inquisitorial squad after hacking, will be removed again after 20 seconds.",
+        "A very interesting and challenging individual project. The app fetches two json files with the use of Asynchronous JavaScript functions, corrects students' names, constructs image sources, calculates blood statuses, and displays it all in browser. In addition, the app allows users to search, filter, and sort students. The most challenging part of the assignment was to implement a function that would hack the system. The function can be called from the console (hackTheSystem();) or by typing 'hackit' while app is open in browser. It adds myself as a student and won't allow my expelling, it also randomizes blood statuses and makes sure that students added to the inquisitorial squad after hacking, will be removed again after 20 seconds.",
       technology: ["HTML", "CSS", "JS"],
       link: "https://lelekaspb.github.io/hogwarts-list/",
     },
